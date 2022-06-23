69eb9e0b976b8ff618f1fa4f12360ac7a3b9420f
Min Lwin <lwin.zawmin@gmail.com>
2022-06-23 16:48:15 +0630

    Share Entity Manager

---
+++ entity-manager/src/main/java/com/jdc/demo/entity/Member.java
@@ -0,0 +1,69 @@
+package com.jdc.demo.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Member implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String loginId;
+	private String password;
+	@Enumerated(EnumType.STRING)
+	private Role role;
+
+	public enum Role {
+		Admin, Member
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLoginId() {
+		return loginId;
+	}
+
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+}
