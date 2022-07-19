19ae907a4e3725b22c3c22c58bb963e5f1ab1c69
Min Lwin <lwin.zawmin@gmail.com>
2022-07-19 23:57:27 +0630

    Using Text Block

--- join-demo/src/test/java/com/jdc/test/ProjectionTest.java
+++ join-demo/src/test/java/com/jdc/test/ProjectionTest.java
@@ -9,7 +9,10 @@ public class ProjectionTest extends AbstractTest{
 	@Test
 	void search_supplier_name_with_product() {
 		
-		var jpql = "select new com.jdc.join.demo.dto.SupplierDto(s.id, s.name, s.phone) from Supplier s join s.product p where p.name = :product";
+		var jpql = """
+				select new com.jdc.join.demo.dto.SupplierDto(s.id, s.name, s.phone) 
+				from Supplier s join s.product p where p.name = :product
+				""";
 		var query = em.createQuery(jpql, SupplierDto.class);
 		query.setParameter("product", "Potato Chips");
 		
